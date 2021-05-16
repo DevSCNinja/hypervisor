bd9d186f2d177543100fde459e0634e7f6594b52
xyz-ctrl <visor@finmail.com>
2021-05-16 16:20:44 +0200

    task

--- tasks/hypervisor.ts
+++ tasks/hypervisor.ts
@@ -21,9 +21,8 @@ task('deploy-hypervisor-factory', 'Deploy Hypervisor contract')
     //TODO cli args
     // goerli
     const args = {
-      uniswapFactory: "0x288be1A33bcdfA9A09cCa95CA1eD628A5294e82c",  
+      uniswapFactory: "0x1f98431c8ad98523631ae4a59f267346ea31f984",  
     };
-
 
     console.log('Network')
     console.log('  ', network.name)
@@ -49,7 +48,7 @@ task('deploy-hypervisor-factory', 'Deploy Hypervisor contract')
       'HypervisorFactory',
       await ethers.getContractFactory('HypervisorFactory'),
       signer,
-      [args]
+      [args.uniswapFactory]
     )
 
     await hypervisorFactory.deployTransaction.wait(5)
@@ -65,8 +64,8 @@ task('verify-factory', 'Deploy Hypervisor contract')
     // TODO cli args
     // goerli
     const args = {
-      uniswapFactory: "0x921156a405461EaE7df9c40D56304258CdDE7016",  
-      factory: "0x921156a405461EaE7df9c40D56304258CdDE7016",  
+      uniswapFactory: "0x1f98431c8ad98523631ae4a59f267346ea31f984",  
+      factory: "0x67C11b788448C149eB08839Af6025Fe6dc80CFbC",  
       token0: "0xFfEc41C97e070Ab5EBeB6E24258B38f69EED5020", 
       token1: "0x1F3BeD559565b56dAabed5790af29ffEd628c4B6",
       fee: FeeAmount.MEDIUM,
@@ -112,7 +111,7 @@ task('deploy-hypervisor', 'Deploy Hypervisor contract')
     // TODO cli args
     // goerli
     const args = {
-      factory: "0x921156a405461EaE7df9c40D56304258CdDE7016",  
+      factory: "0x67C11b788448C149eB08839Af6025Fe6dc80CFbC",  
       token0: "0xFfEc41C97e070Ab5EBeB6E24258B38f69EED5020", 
       token1: "0x1F3BeD559565b56dAabed5790af29ffEd628c4B6",
       fee: FeeAmount.MEDIUM,
@@ -162,4 +161,50 @@ task('deploy-hypervisor', 'Deploy Hypervisor contract')
     const poolAddress = await hypervisor.getPool(args.token0, args.token1, args.fee)
     console.log(poolAddress);
 
-}); 
+  }); 
+
+task('verify-hypervisor', 'Deploy Hypervisor contract')
+  .setAction(async (cliArgs, { ethers, run, network }) => {
+
+    const hypervisorAddress = "0xfb7260e2faE6EF1A33E4Fd5C69E71291C48cA9c7";
+    // TODO cli args
+    // goerli
+    const args = {
+      pool: "0xba9D3f004F7fb378260525cf26F701853CE244eD",
+      owner: signer.address,
+      baseLower: -1800, 
+      baseUpper: 1800, 
+      limitLower: -600, 
+      limitUpper: 0
+    };
+
+    console.log('Network')
+    console.log('  ', network.name)
+    console.log('Task Args')
+    console.log(args)
+console.log(Object.values(args));
+
+    await run('compile')
+
+    // get signer
+
+    const signer = (await ethers.getSigners())[0]
+    console.log('Signer')
+    console.log('  at', signer.address)
+    console.log('  ETH', formatEther(await signer.getBalance()))
+
+    const hypervisor = await ethers.getContractAt(
+      'Hypervisor',
+      hypervisorAddress,
+      signer,
+    )
+
+    // await hypervisor.deployTransaction.wait(5)
+    await run('verify:verify', {
+      address: hypervisor.address,
+      constructorArguments: Object.values(args),
+    })
+
+  });
+
+
