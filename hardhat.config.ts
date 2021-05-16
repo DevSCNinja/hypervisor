977648e30688bc0ee000f117c2cc5982f42d4425
xyz-ctrl <visor@finmail.com>
2021-05-16 16:24:40 +0200

    rm tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import './scripts/copy-uniswap-v3-artifacts.ts'
-import './tasks/hypervisor'
+// import './tasks/hypervisor'
 import { parseUnits } from 'ethers/lib/utils'
 require('dotenv').config()
 const mnemonic = process.env.DEV_MNEMONIC || ''
