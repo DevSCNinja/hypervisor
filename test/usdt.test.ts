7c17a343899310fe1232b9ae8633a22b076adaca
wagekuk <jon.anthony.bowden@icloud.com>
2021-06-07 19:00:37 +0200

    cosmetic changes

--- test/usdt.test.ts
+++ test/usdt.test.ts
@@ -30,9 +30,9 @@ describe('ETHUSDT Hypervisor', () => {
     let factory: UniswapV3Factory
     let uniswapPool: IUniswapV3Pool
     // token0 = WETH9
-    let token0Address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    let wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     // token1 = USDT
-    let token1Address = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    let usdtAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
     let usdcAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
     let uniswapV3Factory = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
     let hypervisorFactory: HypervisorFactory
@@ -45,18 +45,18 @@ describe('ETHUSDT Hypervisor', () => {
     beforeEach('deploy contracts', async () => {
         let hypervisorFactoryFactory = await ethers.getContractFactory('HypervisorFactory')
         hypervisorFactory = (await hypervisorFactoryFactory.deploy(uniswapV3Factory)) as HypervisorFactory
-        await hypervisorFactory.createHypervisor(token0Address, token1Address, FeeAmount.MEDIUM, -1800, 1800, -600, 0)
-        await hypervisorFactory.createHypervisor(usdcAddress, token0Address, FeeAmount.MEDIUM, -1800, 1800, -600, 0)
-        const hypervisorAddress = await hypervisorFactory.getHypervisor(token0Address, token1Address, FeeAmount.MEDIUM)
-        const usdcEthHypervisorAddress = await hypervisorFactory.getHypervisor(usdcAddress, token0Address, FeeAmount.MEDIUM)
+        await hypervisorFactory.createHypervisor(wethAddress, usdtAddress, FeeAmount.MEDIUM, -1800, 1800, -600, 0)
+        await hypervisorFactory.createHypervisor(usdcAddress, wethAddress, FeeAmount.MEDIUM, -1800, 1800, -600, 0)
+        const hypervisorAddress = await hypervisorFactory.getHypervisor(wethAddress, usdtAddress, FeeAmount.MEDIUM)
+        const usdcEthHypervisorAddress = await hypervisorFactory.getHypervisor(usdcAddress, wethAddress, FeeAmount.MEDIUM)
         hypervisor = (await ethers.getContractAt('Hypervisor', hypervisorAddress)) as Hypervisor
         usdcEthHypervisor = (await ethers.getContractAt('Hypervisor', usdcEthHypervisorAddress)) as Hypervisor
 
         factory = (await ethers.getContractAt('UniswapV3Factory', uniswapV3Factory)) as UniswapV3Factory
-        const poolAddress = await factory.getPool(token0Address, token1Address, FeeAmount.MEDIUM)
+        const poolAddress = await factory.getPool(wethAddress, usdtAddress, FeeAmount.MEDIUM)
         uniswapPool = (await ethers.getContractAt('IUniswapV3Pool', poolAddress)) as IUniswapV3Pool
-        usdt = (await ethers.getContractAt('TestERC20', token1Address)) as TestERC20
-        weth9 = (await ethers.getContractAt('TestERC20', token0Address)) as TestERC20
+        usdt = (await ethers.getContractAt('TestERC20', usdtAddress)) as TestERC20
+        weth9 = (await ethers.getContractAt('TestERC20', wethAddress)) as TestERC20
         usdc = (await ethers.getContractAt('TestERC20', usdcAddress)) as TestERC20
     })
 
